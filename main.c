38c65a37d68a6f5b2d5c509659e6ef7925f71c51
Swapnil Diwakar <83010366+listpplo@users.noreply.github.com>
2021-08-04 17:16:30 +0530

    This is blink program for the arduino.

---
+++ main.c
@@ -0,0 +1,21 @@
+#include <avr/io.h>
+#include <util/delay.h>
+ 
+#define BLINK_DELAY_MS 1000
+ 
+int main (void)
+{
+ /* set pin 5 of PORTB for output*/
+ DDRB = DDRB | (1<<DDB5);
+ 
+ while(1) {
+  /* set pin 5 high to turn led on */
+  PORTB = PORTB | (1<<PORTB5);  //set port B
+  _delay_ms(1000);  //wait
+ 
+  /* set pin 5 low to turn led off */
+  PORTB = PORTB & ~(1 <<PORTB5);
+  _delay_ms(1000);
+ }
+}
+
